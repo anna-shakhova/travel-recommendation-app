17fa6ae2167e0b2d6ca3e7eab290b4e8be5e8b06
Anna Shakhova <ashakhova87@gmail.com>
2024-10-31 11:33:37 -0400

    add home page

---
+++ travel_recommendation.js
@@ -0,0 +1,13 @@
+function search() {
+    const searchInput = document.getElementById('search');
+
+    // replace with real code
+    console.log(searchInput.value);
+}
+
+function clearSearch() {
+    const searchInput = document.getElementById('search');
+
+    searchInput.value = '';
+    searchInput.focus();
+}
